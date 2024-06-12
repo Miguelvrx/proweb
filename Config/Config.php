7123077cb4ee72da069508b3736c5597aa904d33
Miguelvrx <miguelangelpotrero70@gnmail.com>
2024-06-12 15:33:37 -0600

    Agregando Configuracion  de la Base de Datos

---
+++ Config/Config.php
@@ -0,0 +1,19 @@
+<?php
+// Definición de constantes para la configuración de la conexión a la base de datos y la URL base de la aplicación
+
+// URL base de la aplicación
+const base_url = "http://localhost/biblio/";
+// Dirección del servidor de base de datos (normalmente 'localhost' para entornos locales)
+const host = "localhost";
+// Nombre de usuario para la conexión a la base de datos
+const user = "kafka";
+// Contraseña para la conexión a la base de datos (probablemente está encriptada o codificada)
+const pass = "5jorgIPQLQy9if4OR5jjidRiYXrbZKoSCdr/DYCsuUc=";
+// Nombre de la base de datos a la que se conectará la aplicación
+const db = "biblioteca_mvc";
+// Conjunto de caracteres a usar en la conexión a la base de datos, aquí se define UTF-8
+const charset = "charset=utf8";
+?>
+
+
+
