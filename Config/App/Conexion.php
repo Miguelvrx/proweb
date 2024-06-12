b5204654b5a3663497738126fc6e46bda28d0449
Miguelvrx <miguelangelpotrero70@gnmail.com>
2024-06-12 15:48:05 -0600

    Agregando la conexion de la base de datos

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -1,18 +1,29 @@
 <?php
-class Conexion{
+// Definición de una clase para manejar la conexión a la base de datos
+class Conexion {
+    // Propiedad privada que almacenará el objeto de conexión PDO
     private $conect;
-    public function __construct()
-    {
+
+    // Constructor de la clase
+    public function __construct() {
+        // Creación del DSN (Data Source Name) para la conexión PDO a la base de datos MySQL
         $pdo = "mysql:host=".host.";dbname=".db.";.charset.";
+
+        // Intento de conexión a la base de datos dentro de un bloque try-catch
         try {
+            // Creación de una nueva instancia de PDO utilizando el DSN, el usuario y la contraseña
             $this->conect = new PDO($pdo, user, pass);
+            // Establecer el modo de error de PDO a excepción, para lanzar errores como excepciones
             $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Error en la conexion".$e->getMessage();
+            // Captura de cualquier excepción que ocurra durante la conexión y muestra un mensaje de error
+            echo "Error en la conexion: " . $e->getMessage();
         }
     }
-    public function conect()
-    {
+
+    // Método público que retorna el objeto de conexión PDO
+    public function conect() {
         return $this->conect;
     }
 }
+?>
