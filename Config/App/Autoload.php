834cccf535c5fe221184c312d1a42861dfaed445
Miguelvrx <miguelangelpotrero70@gnmail.com>
2024-06-20 01:11:43 -0600

    Añadiendo función de autocarga para incluir automáticamente archivos de clases desde el directorio Config/App

---
+++ Config/App/Autoload.php
@@ -0,0 +1,15 @@
+<?php
+// Registramos una función de autocarga utilizando spl_autoload_register
+spl_autoload_register(function($class){
+    
+    // Construimos la ruta del archivo donde se espera que esté definida la clase
+    $file = "Config/App/" . $class . ".php";
+    
+    // Comprobamos si el archivo existe
+    if (file_exists($file)) {
+        
+        // Si el archivo existe, lo incluimos una sola vez
+        require_once $file;
+    }
+});
+?>
