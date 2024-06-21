4d0ad76789ec2dbd30c2d74d25cd3cf6bf3c04b6
Miguelvrx <miguelangelpotrero70@gmail.com>
2024-06-21 13:26:21 -0600

    Definición de la clase Conexion para gestionar la conexión a la base de datos MySQL utilizando PDO

--- Config/App/Autoload.php
+++ Config/App/Autoload.php
@@ -5,7 +5,7 @@ spl_autoload_register(function($class){
     // Construimos la ruta del archivo donde se espera que esté definida la clase
     $file = "Config/App/" . $class . ".php";
     
-    // Comprobamos si el archivo existe
+    // Comprobamos si el archivo existe el php
     if (file_exists($file)) {
         
         // Si el archivo existe, lo incluimos una sola vez
