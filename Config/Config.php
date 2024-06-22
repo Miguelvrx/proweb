c06bea4ef8807e9fdfec88c19bc6d685b4e93bd0
Miguelvrx <miguelangelpotrero70@gmail.com>
2024-06-21 19:00:58 -0600

    Agregando la conexión de la base de datos mediante el archivo Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-// Definición de constantes para la configuración de la conexión a la base de datos y la URL base de la aplicación
+// Definición de constantes para la configuración de la conexión a la base de datos y la URL base de la aplicación del archivo Config.php
 
 // URL base de la aplicación
 const base_url = "http://localhost/biblio/";
