d1d5324069fa3fe589ce697239c318bb777c734d
TLJared <utp0147036@alumno.utpuebla.edu.mx>
2024-07-01 16:07:14 -0600

    Creacion de funcion de insertarEditorial en editorialmodel

--- Models/EditorialModel.php
+++ Models/EditorialModel.php
@@ -12,6 +12,25 @@ class EditorialModel extends Query{
         $res = $this->selectAll($sql);
         return $res;
     }
+
+    public function insertarEditorial($editorial){
+        $verificar = "SELECT * FROM editorial WHERE editorial = '$editorial' ";
+        $existe = $this->select($verificar);
+        if (empty($existe)) {
+            $query = "INSERT INTO editorial(editorial) VALUES (?)";
+            $datos = array($editorial);
+            $data = $this->save($query, $datos);
+            if ($data == 1) {
+                $res = "ok";
+            }else{
+                $res = "error valiste queso UwU";
+            }
+
+        }else{
+            $res = "existe";
+        }
+        return $res;
+    }
 }
 
 ?>
