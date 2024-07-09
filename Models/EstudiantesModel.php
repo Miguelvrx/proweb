de92ad194331580f4ae1df2bc9e969190ed9ab02
TLJared <utp0147036@alumno.utpuebla.edu.mx>
2024-07-08 19:07:18 -0600

    Creacion de funciones que se permite actualizar informacion de estudiantes, ver estado del estudiante, buscar estudiante y verificar permisos

--- Models/EstudiantesModel.php
+++ Models/EstudiantesModel.php
@@ -12,5 +12,40 @@ class EstudianteModel extends Query{
         return $res;
     }
 
+    public function actualizarEstudiante($codigo, $dni, $nombre, $carrera, $direccion, $telefono, $id)
+    {
+        $query = "UPDATE estudiante SET codigo = ?, dni = ?, nombre = ?, carrera = ?, direccion = ?, telefono = ?  WHERE id = ?";
+        $datos = array($codigo, $dni, $nombre, $carrera, $direccion, $telefono, $id);
+        $data = $this->save($query, $datos);
+        if ($data == 1) {
+            $res = "modificado";
+        } else {
+            $res = "error";
+        }
+        return $res;
+    }
+    public function estadoEstudiante($estado, $id)
+    {
+        $query = "UPDATE estudiante SET estado = ? WHERE id = ?";
+        $datos = array($estado, $id);
+        $data = $this->save($query, $datos);
+        return $data;
+    }
+    public function buscarEstudiante($valor)
+    {
+        $sql = "SELECT id, codigo, nombre AS text FROM estudiante WHERE codigo LIKE '%" . $valor . "%' AND estado = 1 OR nombre LIKE '%" . $valor . "%'  AND estado = 1 LIMIT 10";
+        $data = $this->selectAll($sql);
+        return $data;
+    }
+    public function verificarPermisos($id_user, $permiso)
+    {
+        $tiene = false;
+        $sql = "SELECT p.*, d.* FROM permisos p INNER JOIN detalle_permisos d ON p.id = d.id_permiso WHERE d.id_usuario = $id_user AND p.nombre = '$permiso'";
+        $existe = $this->select($sql);
+        if ($existe != null || $existe != "") {
+            $tiene = true;
+        }
+        return $tiene;
+    }
 }
 ?>
