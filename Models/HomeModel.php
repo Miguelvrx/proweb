01013e44e2b97a9cd0830a75d0bb79d3417e013b
TLJared <utp0147036@alumno.utpuebla.edu.mx>
2024-07-15 17:35:58 -0600

    Creacion de diferentes funcionales como actualizarLibro, BuscarLibro, VerficarPermisos,

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -28,5 +28,41 @@ class LibroModel extends Query{
         }
 
     }
+
+    public function editLibros($id)
+    {
+        $sql = "SELECT * FROM libro WHERE id = $id";
+        $res = $this->select($sql);
+        return $res;
+    }
+
+    function actualizarLibro($titulo, $id_autor, $id_editorial, $id_materia, $cantidad, $num_pagina, $anio_edicion, $descripcion, $imgNombre){
+        $query = "UPDATE libro SET titulo = ?, id_autor  = ?,id_editorial  = ?, id_materia  = ?,cantidad  = ?,num_pagina  = ?,anio_edicion  = ?,descripcion  = ?,imagen  = ? WHERE id = ?";
+        $datos = array($titulo, $id_autor, $id_editorial, $id_materia, $cantidad, $num_pagina, $anio_edicion, $descripcion, $imgNombre);
+        $data = $this->save($query,$datos);
+        if ($data == 1) {
+            $res = "Modificado";
+        } else{
+            $res = "error";
+        }
+        return $res;
+    }
+
+    public function buscarLibro($valor){
+        $sql = "SELECT id, titulo AS text FROM libro WHERE titulo LIKE '%". $valor . "%' AND estado = LIMIT 10";
+        $data = $this->selectAll($sql);
+        return $data;
+    }
+
+    public function VerficarPermisos($id_user, $permiso){
+        $tiene = false;
+        $sql = "SELECT p.*,d.* FROM permisos p INNER JOIN detalle_permisos d ON p.id = d.id_permisos WHERE d.id_usuario = $id_user AND p.nombre = '$permiso'";
+        $existe = $this->selectAll($sql);
+        if ($existe != null) {
+            $tiene = true;
+        }
+        return $tiene;
+    }
+
 }
 ?>
