c50a1fbf9107244a6a4f1f2748ac8b2b066bb678
TLJared <utp0147036@alumno.utpuebla.edu.mx>
2024-07-17 10:12:39 -0600

    creacion de funciones de listar libros, registrar y editar libro

--- Controllers/Libros.php
+++ Controllers/Libros.php
@@ -17,5 +17,105 @@ class Libros extends Controller{
     public function index(){
         $this->views->getView($this, "index");
     }
+
+    public function listar()
+    {
+        $data = $this->model->getLibros();
+        for ($i = 0; $i < count($data); $i++) {
+            $data[$i]['foto'] = '<img class="img-thumbnail" src="' . base_url . "Assets/img/libros/" . $data[$i]['imagen'] . '" width="100">';
+            if ($data[$i]['estado'] == 1) {
+                $data[$i]['estado'] = '<span class="badge badge-success">Ativo</span>';
+                $data[$i]['acciones'] = '<div class="d-flex">
+                <button class="btn btn-primary" type="button" onclick="btnEditarLibro(' . $data[$i]['id'] . ');"><i class="fa fa-pencil-square-o"></i></button>
+                <button class="btn btn-danger" type="button" onclick="btnEliminarLibro(' . $data[$i]['id'] . ');"><i class="fa fa-trash-o"></i></button>
+                <div/>';
+            } else {
+                $data[$i]['estado'] = '<span class="badge badge-danger">Inativo</span>';
+                $data[$i]['acciones'] = '<div>
+                <button class="btn btn-success" type="button" onclick="btnReingresarLibro(' . $data[$i]['id'] . ');"><i class="fa fa-reply-all"></i></button>
+                <div/>';
+            }
+        }
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    public function registrar()
+    {
+        $titulo = strClean($_POST['titulo']);
+        $autor = strClean($_POST['autor']);
+        $editorial = strClean($_POST['editorial']);
+        $materia = strClean($_POST['materia']);
+        $cantidad = strClean($_POST['cantidad']);
+        $num_pagina = strClean($_POST['num_pagina']);
+        $anio_edicion = strClean($_POST['anio_edicion']);
+        $descripcion = strClean($_POST['descripcion']);
+        $id = strClean($_POST['id']);
+        $img = $_FILES['imagen'];
+        $name = $img['name'];
+        $fecha = date("YmdHis");
+        $tmpName = $img['tmp_name'];
+        if (empty($titulo) || empty($autor) || empty($editorial) || empty($materia) || empty($cantidad)) {
+            $msg = array('msg' => 'Todos os campos são exigidos!', 'icono' => 'warning');
+        } else {
+            if (!empty($name)) {
+                $extension = pathinfo($name, PATHINFO_EXTENSION);
+                $formatos_permitidos =  array('png', 'jpeg', 'jpg');
+                $extension = pathinfo($name, PATHINFO_EXTENSION);
+                if (!in_array($extension, $formatos_permitidos)) {
+                    $msg = array('msg' => 'Tipo de arquivo não permitido!', 'icono' => 'warning');
+                } else {
+                    $imgNombre = $fecha . ".jpg";
+                    $destino = "Assets/img/libros/" . $imgNombre;
+                }
+            } else if (!empty($_POST['foto_actual']) && empty($name)) {
+                $imgNombre = $_POST['foto_actual'];
+            } else {
+                $imgNombre = "logo.png";
+            }
+            if ($id == "") {
+                $data = $this->model->insertarLibros($titulo, $autor, $editorial, $materia, $cantidad, $num_pagina, $anio_edicion, $descripcion, $imgNombre);
+                if ($data == "ok") {
+                    if (!empty($name)) {
+                        move_uploaded_file($tmpName, $destino);
+                    }
+                    $msg = array('msg' => 'Livro cadastrado com sucesso!', 'icono' => 'success');
+                } else if ($data == "existe") {
+                    $msg = array('msg' => 'Este livro já existe!', 'icono' => 'warning');
+                } else {
+                    $msg = array('msg' => 'Erro ao cadastrar o livro!', 'icono' => 'error');
+                }
+            } else {
+                $imgDelete = $this->model->editLibros($id);
+                if ($imgDelete['imagen'] != 'logo.png') {
+                    if (file_exists("Assets/img/libros/" . $imgDelete['imagen'])) {
+                        unlink("Assets/img/libros/" . $imgDelete['imagen']);
+                    }
+                }
+                $data = $this->model->actualizarLibros($titulo, $autor, $editorial, $materia, $cantidad, $num_pagina, $anio_edicion, $descripcion, $imgNombre, $id);
+                if ($data == "modificado") {
+                    if (!empty($name)) {
+                        move_uploaded_file($tmpName, $destino);
+                    }
+                    $msg = array('msg' => 'Livro alterado com sucesso!', 'icono' => 'success');
+                } else {
+                    $msg = array('msg' => 'Erro ao alterar o livro!', 'icono' => 'error');
+                }
+            }
+        }
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    public function editar($id)
+    {
+        $data = $this->model->editLibros($id);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+
+
+
 }
 ?>
